6fce4a8801cef2ea6357f1c7d47de7624a9c7ab7
Kyle Matter <kylematter@ucla.edu>
2016-11-21 00:23:06 +0000

    added more pages

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -1,4 +1,10 @@
 class GeneralController < ApplicationController
   def index
   end
+  
+  def about
+  end
+  
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'general/index'
   
   root 'general#index'
+  get '/about' => 'general#about'
+  get '/contact' => 'general#contact'
+  get '/news/show' => 'news#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
