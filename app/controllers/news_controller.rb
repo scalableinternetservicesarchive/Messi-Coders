a787a1576fb38dee685cb9f4ac9422996e56697a
Kyle Matter <kylematter@ucla.edu>
2016-11-23 00:48:58 +0000

    editing and deleting news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,13 +3,34 @@ class NewsController < ApplicationController
         @news = News.all
     end
     
+    def show
+        @news = News.find(params[:id])
+    end
+    
     def new
+        @news = News.new
+    end
+    
+    def edit
+        @news =  News.find(params[:id])
     end
     
     def create
         @news = News.new(news_params)
         @news.save
         redirect_to action: "index"
+    end
+    
+    def update
+        @news = News.find(params[:id])
+        @news.update(news_params)
+        redirect_to action: "index"
+    end
+    
+    def destroy
+        @news = News.find(params[:id])
+        @news.destroy
+        redirect_to news_index_path
     end
     
     private
