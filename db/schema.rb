14d58200bf52be652987529cb3738d519cd3e514
cmathews95 <mathews.christie@gmail.com>
2016-11-28 21:30:29 -0800

    Created Postgres Databases and script to run postgres server

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161129044407) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
