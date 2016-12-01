dc9abf4f81b0dcccba6fc981c754f6fc2866abb6
M. Fay Wu <mfaywu@ucla.edu>
2016-12-01 03:05:08 -0800

    take out auth token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
   require 'csv'
 
     # @method getFiles - Returns csv files the player is found inside
